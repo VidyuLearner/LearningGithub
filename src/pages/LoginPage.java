2cd524a1005db6ae988a89625d7722b5dc5d65b6
VidyuLearner <y.vidyu@gmail.com>
2023-08-19 06:32:15 +0800

    added/updated/deleted1

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("Login Page");
+		String UserName = "Admin";
+		String Pwd = "password";
+		System.out.println("Admin logged in");
 	}
 
 }
