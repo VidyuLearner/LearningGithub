725705fdb21db79aad0d3b7157693742ce0d3617
VidyuLearner <y.vidyu@gmail.com>
2023-10-03 05:42:24 +0800

    added orderpage class

--- src/pages/AddingUntracked.java
+++ src/pages/AddingUntracked.java
@@ -4,6 +4,7 @@ public class AddingUntracked {
 
 	public static void main(String[] args) {
 		System.out.println("commiting untracked file");
+		System.out.println("adding one more commit for practice");
 	}
 
 }
