72d4f2f66ee76120b185a93b17e4d5b0f2c9c1ed
VidyuLearner <y.vidyu@gmail.com>
2023-10-03 05:58:56 +0800

    updated orderpage as per lead comments

--- src/pages/OrderPage.java
+++ src/pages/OrderPage.java
@@ -4,6 +4,9 @@ public class OrderPage {
 	
 	public static void main(String[] args) {
 		System.out.println("Order Page");
+		System.out.println("get price");
+		System.out.println("result profit");
+		
 	}
 	
 
