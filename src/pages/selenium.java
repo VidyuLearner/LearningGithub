a454f6925812842e0ce7a17452b80a398da3a75d
VidyuLearner <127169160+VidyuLearner@users.noreply.github.com>
2023-10-03 12:41:40 +0800

    Create selenium.java

---
+++ src/pages/selenium.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class selenium {
+
+	public static void main(String[] args) {
+		System.out.println("commiting untracked file");
+		System.out.println("adding one more commit for practice");
+	}
+
+}
